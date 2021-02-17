48881080441cb8d225777df57cac51fbebc84670
casey-collins <76925085+casey-collins@users.noreply.github.com>
2021-02-16 20:54:42 -0500

    Add files via upload

--- artWeb.js
+++ artWeb.js
@@ -1,16 +1,8 @@
-/*JS for the Modal on the Main Page*/
-/*document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });*/
-
-/*JS for the Responsive Navbar */
 document.addEventListener('DOMContentLoaded', function() {
     const elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems, null);
 });
 
-/*JS for the Carousel on the gallery page*/
 document.addEventListener('DOMContentLoaded', function() {
     const elems = document.querySelectorAll('.carousel');
     const numVisible = 5;
